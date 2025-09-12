997dee03649969730ce220cd87f237ff09c4f892
NatNichols <natalie.e.nichols@gmail.com>
2025-09-12 18:19:35 -0400

    build out create groceryItem fxn

--- controllers/groceryItems.js
+++ controllers/groceryItems.js
@@ -1,7 +1,15 @@
 import { GroceryItem } from '../models/groceryItem.js'
 
-
+export async function create(req, res) {
+  try {
+    const groceryItem = await GroceryItem.create(req.body)
+    res.status(201).json(groceryItem)
+  } catch (err) {
+    console.log(`🚨`, err)
+    res.status(500).json(`🚨`, err)
+  }
+}
 
 export {
-  
+
 }
--- routes/groceryItems.js
+++ routes/groceryItems.js
@@ -4,7 +4,8 @@ import * as groceryItemsCtrl from '../controllers/groceryItems.js'
 const router = Router()
 
 // ========== Public Routes ===========
-
+// POST /api/groceryItems
+router.post('/', groceryItemsCtrl.create)
 
 export {
   router
